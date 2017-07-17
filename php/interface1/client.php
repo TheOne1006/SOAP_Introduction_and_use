8fad04bb6392a48b7a7770a1108f649b72abc1cd
theone <297190869@qq.com>
2017-07-17 16:48:21 +0800

    案例增加 公司 demo, 但是安全问题, 不予以上传

--- php/interface1/client.php
+++ php/interface1/client.php
@@ -4,6 +4,18 @@ ini_set('soap.wsdl_cache_enabled',0);
 ini_set('soap.wsdl_cache_ttl',0);
 
 $soap = new SoapClient('http://localhost/interface1/soap.wsdl');
+
+echo ("SOAP服务器提供的开放函数:");
+echo ('<pre>');
+var_dump ( $soap->__getFunctions () );//获取服务器上提供的方法
+echo ('</pre>');
+echo ("SOAP服务器提供的Type:");
+echo ('<pre>');
+var_dump ( $soap->__getTypes () );//获取服务器上数据类型
+echo ('</pre>');
+
+echo '<pre>';
 var_dump($soap);
+echo '</pre>';
 echo $soap->Add(1,2);
 ?>
